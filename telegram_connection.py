a695a66df50732d86dd37ee33819115aee6ce6e3
YAAKOV BRIN <ybrin@g.jct.ac.il>
2021-08-15 14:26:14 +0300

    basic telegram bot handler

--- telegram_connection.py
+++ telegram_connection.py
@@ -73,24 +73,34 @@ def start(message):
 
 
 @bot.message_handler(content_types=['text'])
-def welcome(pm):
-    #sent_msg = view_server.start_message()
-    sent_msg = bot.send_message(pm.chat.id, view_server.start_message())
-    bot.register_next_step_handler(sent_msg, name_handler)  # Next message will call the name_handler function
+def msg_handler(pm):
+    sender_name = " "
+    msg_text = pm.text
+    switcher = {
+            0: view_server.start_message(),
+            1: view_server.add_id(sender_name, msg_text),
+            2: view_server.add_birth_year(sender_name, msg_text),
+            3: view_server.add_type_of_doctors(sender_name, msg_text),
+            4: view_server.all_is_fill(sender_name, msg_text)
+        }
+    rep_str = switcher.get(view_server.get_connection_status(), "I do not understand you.")
+    sent_msg = bot.send_message(pm.chat.id, rep_str)
+    bot.send_message(pm.chat.id, sent_msg)
+    #bot.register_next_step_handler(sent_msg, name_handler)  # Next message will call the name_handler function
 
 
-def name_handler(pm):
-    id = pm.text
-
-    sent_msg = bot.send_message(pm.chat.id, view_server.add_id(id))
-
-    bot.register_next_step_handler(sent_msg, age_handler, id)  # Next message will call the age_handler function
-
-
-def age_handler(pm, id):
-    year = pm.text
-    bot.send_message(pm.chat.id, f"Your name is {id}, and your age is {year}.")
-    #automation_fill.run_web(id,year)
+# def name_handler(pm):
+#     id = pm.text
+#
+#     sent_msg = bot.send_message(pm.chat.id, view_server.add_id(id))
+#
+#     bot.register_next_step_handler(sent_msg, age_handler, id)  # Next message will call the age_handler function
+#
+#
+# def age_handler(pm, id):
+#     year = pm.text
+#     bot.send_message(pm.chat.id, f"Your name is {id}, and your age is {year}.")
+#     #automation_fill.run_web(id,year)
 
 
 

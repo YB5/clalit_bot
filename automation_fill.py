f3a609239c42921e12abafcdda0aef9a1b57e4af
yaron <yarongerstel@windowslive.com>
2021-08-15 14:11:28 +0300

    starting automation_fill.py

--- automation_fill.py
+++ automation_fill.py
@@ -7,33 +7,23 @@ from selenium.webdriver.support import expected_conditions as EC
 
 import time
 
-id = '311221790'
-yearborne = '1993'
 
+def run_web(id,year):
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get('https://e-services.clalit.co.il/onlinewebquick/nvgq/tamuz/he-il')
 
+    idbox = driver.find_element_by_xpath('//*[@id="ctl00_ctl00_cphBody_bodyContent_ucQuickLogin_userId"]')
+    idbox.send_keys(id)
 
+    yearbornebox = driver.find_element_by_xpath('//*[@id="ctl00_ctl00_cphBody_bodyContent_ucQuickLogin_userYearOfBirth"]')
+    yearbornebox.send_keys(year)
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-wait = ui.WebDriverWait(driver, 10)
-driver.get('https://e-services.clalit.co.il/onlinewebquick/nvgq/tamuz/he-il')
+    moveonbutten = driver.find_element_by_xpath('//*[@id="ctl00_ctl00_cphBody_bodyContent_ucQuickLogin_btnLogin_lblInnerText"]')
+    moveonbutten.click()
 
-idbox = driver.find_element_by_xpath('//*[@id="ctl00_ctl00_cphBody_bodyContent_ucQuickLogin_userId"]')
-idbox.send_keys(id)
+    time.sleep(5)
+    ProfessionVisitButton = driver.find_element_by_link_text("לרופאים יועצים").click()
 
-yearbornebox = driver.find_element_by_xpath('//*[@id="ctl00_ctl00_cphBody_bodyContent_ucQuickLogin_userYearOfBirth"]')
-yearbornebox.send_keys(yearborne)
-
-moveonbutten = driver.find_element_by_xpath('//*[@id="ctl00_ctl00_cphBody_bodyContent_ucQuickLogin_btnLogin_lblInnerText"]').click()
-
-
-#window_after = driver.window_handles[1]
-#driver.switch_to.window(window_after)
-#driver.get('https://e-services.clalit.co.il/OnlineWebQuick/QuickServices/Tamuz/TamuzTransfer.aspx')
-#time.sleep(8)
-#wait.until(lambda driver: driver.find_element_by_xpath('//*[@id="ProfessionVisitButton"]'))
-#driver.execute_script("window.scrollTo(0, 1800)")
-time.sleep(5)
-ProfessionVisitButton = driver.find_element_by_xpath('//*[@id="ProfessionVisitButton"]').click()
-
+#run_web("311221790","1993")
 
 
--- telegram_connection.py
+++ telegram_connection.py
@@ -1,62 +1,98 @@
-"""
-interacting with the Telegram API and create a Flask server setup.
-
-"""
-from flask import Flask, Response, request
-import requests as requests
+# """
+# interacting with the Telegram API and create a Flask server setup.
+#
+# """
+# from flask import Flask, Response, request
+# import requests as requests
+# import view_server
+#
+#
+# app = Flask(__name__)
+#
+#
+# # Telegram API
+# TOKEN = '1943783961:AAEZlvH1DPwdw4yrCxM6XgDMImlsHHhQGOQ'
+# TELEGRAM_INIT_WEBHOOK_URL = 'https://api.telegram.org/bot{}/setWebhook?url=' \
+#                             'http://990394205607.ngrok.io/message'.format(TOKEN)
+#
+# requests.get(TELEGRAM_INIT_WEBHOOK_URL)
+#
+# @app.route('/sanity')
+# def sanity():
+#     """
+#     sanity check for the flask server
+#     :return: str message
+#     """
+#     return "Server is running"
+#
+#
+# @app.route('/message', methods=["POST"])
+# def handle_message():
+#     """
+#     Every message sent in a bot activates this function.
+#
+#     From the json file, extract the message, sender name, and chat id.
+#     Interpret the message.
+#     Send a reply message via telegram api.
+#
+#     :return: Response. status of the reply message
+#     """
+#
+#     resp = request.get_json()
+#     if resp.get("message", "") == "":
+#         return Response("success")
+#     msg_text = resp["message"]["text"]
+#     sender_name = resp["message"]["from"]["first_name"]
+#     chat_id = resp["message"]["chat"]["id"]
+#     switcher = {
+#         0: view_server.start_message(),
+#         1: view_server.add_id(sender_name, msg_text),
+#         2: view_server.add_birth_year(sender_name, msg_text),
+#         3: view_server.add_type_of_doctors(sender_name, msg_text),
+#         4: view_server.all_is_fill(sender_name, msg_text)
+#     }
+#     rep_str = switcher.get(view_server.get_connection_status(), "I do not understand you.")
+#     res = requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(TOKEN, chat_id, rep_str))
+#     if res.status_code == 200:
+#         return Response("success")
+#     return Response("fail")
+#
+#
+# if __name__ == '__main__':
+#     app.run(port=9002)
+import telebot
+import automation_fill
 import view_server
+API_KEY = "1919382703:AAErQuc42X-pjIW7OrR3a3CsHc0Yd-s-ybk"
+
+bot = telebot.TeleBot(API_KEY)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message,"hey! \n what your id? ")
 
 
-app = Flask(__name__)
+@bot.message_handler(content_types=['text'])
+def welcome(pm):
+    #sent_msg = view_server.start_message()
+    sent_msg = bot.send_message(pm.chat.id, view_server.start_message())
+    bot.register_next_step_handler(sent_msg, name_handler)  # Next message will call the name_handler function
 
 
-# Telegram API
-TOKEN = '1943783961:AAEZlvH1DPwdw4yrCxM6XgDMImlsHHhQGOQ'
-TELEGRAM_INIT_WEBHOOK_URL = 'https://api.telegram.org/bot{}/setWebhook?url=' \
-                            'http://990394205607.ngrok.io/message'.format(TOKEN)
+def name_handler(pm):
+    id = pm.text
 
-requests.get(TELEGRAM_INIT_WEBHOOK_URL)
+    sent_msg = bot.send_message(pm.chat.id, view_server.add_id(id))
 
-@app.route('/sanity')
-def sanity():
-    """
-    sanity check for the flask server
-    :return: str message
-    """
-    return "Server is running"
+    bot.register_next_step_handler(sent_msg, age_handler, id)  # Next message will call the age_handler function
 
 
-@app.route('/message', methods=["POST"])
-def handle_message():
-    """
-    Every message sent in a bot activates this function.
-
-    From the json file, extract the message, sender name, and chat id.
-    Interpret the message.
-    Send a reply message via telegram api.
-
-    :return: Response. status of the reply message
-    """
-
-    resp = request.get_json()
-    if resp.get("message", "") == "":
-        return Response("success")
-    msg_text = resp["message"]["text"]
-    sender_name = resp["message"]["from"]["first_name"]
-    chat_id = resp["message"]["chat"]["id"]
-    switcher = {
-        0: view_server.start_message(),
-        1: view_server.add_id(sender_name, msg_text),
-        2: view_server.add_birth_year(sender_name, msg_text),
-        3: view_server.add_type_of_doctors(sender_name, msg_text),
-        4: view_server.all_is_fill(sender_name, msg_text)
-    }
-    rep_str = switcher.get(view_server.get_connection_status(), "I do not understand you.")
-    res = requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(TOKEN, chat_id, rep_str))
-    if res.status_code == 200:
-        return Response("success")
-    return Response("fail")
+def age_handler(pm, id):
+    year = pm.text
+    bot.send_message(pm.chat.id, f"Your name is {id}, and your age is {year}.")
+    #automation_fill.run_web(id,year)
 
 
-if __name__ == '__main__':
-    app.run(port=9002)
+
+
+bot.polling()

2ff5cd1e8d60dded532f5306850e85f6ac628ab1
YAAKOV BRIN <ybrin@g.jct.ac.il>
2021-08-16 11:26:13 +0300

    server redis 1

--- data_server.py
+++ data_server.py
@@ -1,8 +1,47 @@
-ID = 0
-BirthYear = 1900
-Specialty = ""
-START_MESSAGE = 1
-ID_FILL = 2
-BIRTHYEAR_FILL = 3
-SPECIALTY_FILL = 4
-connection_status = 0
+# ID = 0
+# BirthYear = 1900
+# Specialty = ""
+# START_MESSAGE = 1
+# ID_FILL = 2
+# BIRTHYEAR_FILL = 3
+# SPECIALTY_FILL = 4
+# connection_status = 0
+"""
+redis db connection & actions
+"""
+import redis
+
+_r_server = redis.Redis(
+    host='localhost',
+    port='6379')
+
+
+def r_set(k, v):
+    """
+    enter item to db
+    if k already define, it is override.
+    :param k: key to enter
+    :param v: value to enter
+    :return: "ok" if it was executed correctly.
+    """
+    return _r_server.set(k, v)
+
+
+def r_get(k: str):
+    """
+    get value by key.
+    :param k: key
+    :return: the value of key, or nil when key does not exist.
+    """
+    return _r_server.get(k)
+
+
+def init():
+    _r_server.set("ID", "0")
+    _r_server.set("BirthYear", "1900")
+    _r_server.set("Specialty", "")
+    _r_server.set("START_MESSAGE", 1)
+    _r_server.set("ID_FILL", 2)
+    _r_server.set("BIRTHYEAR_FILL", 3)
+    _r_server.set("SPECIALTY_FILL", 4)
+    _r_server.set("connection_status", 0)
--- server_controller.py
+++ server_controller.py
@@ -4,52 +4,54 @@ import automation_fill
 
 
 def get_connection_status():
-    return data_server.connection_status
+    return data_server.r_get("connection_status").connection_status
 
 
 def set_connection_status(status):
-    if status == data_server.connection_status + 1:
-        data_server.connection_status = status
+    if status == data_server._r_server("connection_status") + 1:
+        data_server.r_set("connection_status", status)
 
 
 def set_id(new_id):
     if new_id.isnumeric():
-        data_server.ID = new_id
-        set_connection_status(data_server.ID_FILL)
+        data_server.r_set("ID", new_id)
+        set_connection_status(data_server.r_get("ID_FILL"))
         return True
     return False
 
 
 def get_id():
-    return data_server.ID
+    return data_server.r_get("ID")
 
 
 def set_year(year):
     if year.isnumeric() and int(year) > 1901 and int(year) < date.today().year:
-        data_server.BirthYear = year
-        set_connection_status(data_server.BIRTHYEAR_FILL)
+        data_server.r_set("BirthYear", year)
+        set_connection_status(data_server.r_get("BIRTHYEAR_FILL"))
         return True
     return False
 
 
 def get_year(year):
-    return data_server.BirthYear
+    return data_server.r_get("BirthYear")
 
 
 def set_start_message():
-    set_connection_status(data_server.START_MESSAGE)
+    set_connection_status(data_server.r_get("START_MESSAGE"))
 
 
 def find_appointment():
-    #todo date check
-    return automation_fill.run_web(data_server.ID,data_server.BirthYear,data_server.Specialty)
-
+    # todo date check
+    return automation_fill.run_web(data_server.r_get("ID"), data_server.r_get("BirthYear"), data_server.r_get("Specialty"))
 
 
 def add_specialty(msg_text):
-    specialty = ["orthopedics", "otolaryngology", "women"]
+    specialty = ["orthopedics", "otolaryngology", "breast surgeon", "women", "skin", "eyes"]
     if any([msg_text == x for x in specialty]):
-        data_server.Specialty = msg_text
-        set_connection_status(data_server.SPECIALTY_FILL)
+        data_server.r_set("Specialty", msg_text)
+        set_connection_status(data_server.r_get("SPECIALTY_FILL"))
         return True
     return False
+
+
+data_server.init()
